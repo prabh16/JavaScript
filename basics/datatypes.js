32df00eacae7f49965f038cb9c4032aff8d9cf6b
Prabhnoor <114477888+prabh16@users.noreply.github.com>
2024-09-10 18:16:52 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3 + 3) we are using nodejs not browser
+
+console.log(3
+    + 
+    3) //code readability should be high
+
+console.log("abc");
+
+let name = "abc"
+let age = 19
+let isLoggedIn = true
+let state;
+
+// number => 2 raised to the power of 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
